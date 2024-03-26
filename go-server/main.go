6debbe0f79ba5d5895deb91af686dde1506ea6bf
raj-kariya <rajkariya2003@gmail.com>
2024-03-26 19:07:42 +0530

    Submit button Not working

--- go-server/main.go
+++ go-server/main.go
@@ -26,6 +26,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request){
 	}
 	if r.Method != "GET"{
 		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
 	}
 	fmt.Fprintf(w, "hello")
 }
